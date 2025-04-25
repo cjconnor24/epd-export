559bfd401039d44f84a98674e90b875e70228cfc
Chris Connor <chris.connor@comcarde.com>
2025-04-25 09:09:34 +0100

    Adding intial script and the bnf_code file to search

---
+++ extract_bnf_code.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+
+if [ $# -lt 1 ];then
+    echo "You need to pass at least one file name; e.g. epd_202411.csv"
+    exit 1
+fi
+
+if command -v rg >/dev/null 2>&1; then
+    echo "ripgrep is installed...continuing"
+else
+    echo "ripgrep is not installed. brew install ripgrep or apt install ripgrep"
+    exit 1
+fi
+
+
+# Check the bnf code list
+BNF_CODE_LIST=$(cat bnf_codes.txt)
+BNF_PATTERN=$(echo "$BNF_CODE_LIST" | xargs | sed -E 's/ /|/g')
+echo -e "\nDetected $(echo "$BNF_CODE_LIST" | wc -l) BNF codes in bnf_codes.txt\n"
+
+for fname in "$@"; do
+    
+    echo -e "\nProcessing: $fname"
+
+
+    if [ ! -f "$fname" ];then
+        echo "File $fname does not exist"
+        continue
+    fi
+
+    echo "Searching for BNF codes in $fname..."
+
+    TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+    FILE_NAME="$(echo $fname | sed 's/.csv//g')_extracted_$TIMESTAMP.csv"
+    CODES=$(rg "$BNF_PATTERN" "./$fname")
+   
+    if [ -z "$CODES" ]; then
+        echo -e "No BNF codes found in $fname\n"
+        continue
+    fi
+
+    echo "$CODES" > "$FILE_NAME"
+
+    EXTRACTED_COUNT=$(wc -l < "$FILE_NAME")
+
+    echo "$EXTRACTED_COUNT BNF codes extracted from $fname and saved to $FILE_NAME"
+
+
+done
